2311a407d3d3c298315fc69511d9044cde43b907
Tom <thomasjhaughton@hotmail.com>
2017-05-04 14:14:15 +0100

    Initial Commit

---
+++ TravellingSalesPerson/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  TravellingSalesPerson
+//
+//  Created by Thomas Haughton on 04/05/2017.
+//  Copyright © 2017 Thomas Haughton. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
